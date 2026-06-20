89dc7f4f0bfead97d0f51991a65e541e2a210fc5
Ayush Bhardwaj <ayush.bhardwaj@example.com>
2026-06-20 23:56:20 +0530

    Fix landing overlay texts overlapping/merging on load

--- src/components/utils/initialFX.ts
+++ src/components/utils/initialFX.ts
@@ -12,6 +12,8 @@ export function initialFX() {
     delay: 1,
   });
 
+  gsap.set([".landing-h2-info-1", ".landing-h2-2"], { opacity: 1 });
+
   var landingText = new SplitText(
     [".landing-info h3", ".landing-intro h2", ".landing-intro h1"],
     {
